5a68bbaf6b66399a0163cbd775e1a1c7055ad707
Shaunak Kale <epicshaunak@gmail.com>
2017-10-14 14:35:08 -0700

    hi

--- index.js
+++ index.js
@@ -2,52 +2,63 @@ var express = require('express'),
   app = express(),
   fs = require('fs'),
   sys = require('sys'),
-  twitter = require('node-tweet-stream')
-
-
-var t = new twitter({
+  twitter = require('node-tweet-stream'),
+  stock = "",
+  request = require('request')
+var Twitter = require('twitter');
+var client = new Twitter({
   consumer_key: 'SdhqyogLOTVt7QJjBcXTbsBHE',
   consumer_secret: 's73WmhkPOm3QcgAM2Yozfn5CelQ36sYoQmdBmKjuVNcJeAlVXk',
-  token: '3303757363-SqSh0Ol7lXzT9lSoPBUDC2flnpTCYF4ORjtE91h',
-  token_secret: 'bgyjEfAZGBFlok9uSp3vJg67VCw1rGkw0FecLo9THs6Eg'
+  access_token_key: '3303757363-SqSh0Ol7lXzT9lSoPBUDC2flnpTCYF4ORjtE91h',
+  access_token_secret: 'bgyjEfAZGBFlok9uSp3vJg67VCw1rGkw0FecLo9THs6Eg'
 });
 
-t.on('tweet', function(tweet) {
-  console.log('tweet received', tweet)
-  io.emit(tweet);
-})
+var sentiment = require('sentiment');
+
+var params = {
+  screen_name: 'nodejs'
+};
 
 
-t.on('error', function(err) {
-  console.log('Oh no')
-})
 
-t.track('stock')
+
+
+
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/views'));
-
-// views is directory for all template files
 app.set('views', __dirname + '/build');
-
 app.get('/', function(request, response) {
   response.sendFile('/views/index.html');
 });
 app.get('/stock', function(request, response) {
   response.sendFile('/views/index.html');
 });
-
 server = app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+var str
+var sent
 io = require('socket.io')(server);
 io.on('connection', function(socket) {
   console.log('A user connected');
 
   socket.on('stock', function(data) {
-    console.log(data)
-  });
-  socket.on('disconnect', function() {
-    console.log('A user disconnected');
-  });
+    str = data.toString()
+    client.get('search/tweets.json', {
+      q: str,
+      count: 1000
+    }, function(error, tweets, response) {
+      if (tweets.statuses.length) {
+        console.log(JSON.stringify(tweets));
+        for (i = 0; i < tweets.statuses.length; i++) {
+          sent = sentiment(tweets.statuses[i].text)
+          tweets.statuses[i].sentiment = sent
+        }
+        socket.emit("tweet", tweets)
+      }
+    });
+    console.log(str)
 
-});
+  });
+})
