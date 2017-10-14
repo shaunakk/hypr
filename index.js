f7cfc314b54cb6d2ddaeedcaa7403ae2ab87d982
Shaunak Kale <epicshaunak@gmail.com>
2017-10-14 03:48:10 -0700

    i

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-var express = require('express');
-var app = express();
-app.set('port', (process.env.PORT || 5000));
+var express = require('express'),
+  app = express(),
+  fs = require('fs'),
+  sys = require('sys'),
+  twitter = require('node-tweet-stream')
 
+
+var t = new twitter({
+  consumer_key: 'SdhqyogLOTVt7QJjBcXTbsBHE',
+  consumer_secret: 's73WmhkPOm3QcgAM2Yozfn5CelQ36sYoQmdBmKjuVNcJeAlVXk',
+  token: '3303757363-SqSh0Ol7lXzT9lSoPBUDC2flnpTCYF4ORjtE91h',
+  token_secret: 'bgyjEfAZGBFlok9uSp3vJg67VCw1rGkw0FecLo9THs6Eg'
+});
+
+t.on('tweet', function(tweet) {
+  console.log('tweet received', tweet)
+  io.emit(tweet);
+})
+
+t.on('error', function(err) {
+  console.log('Oh no')
+})
+
+t.track('stock')
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/views'));
 
 // views is directory for all template files
@@ -10,7 +31,11 @@ app.set('views', __dirname + '/build');
 app.get('/', function(request, response) {
   response.sendFile('/views/index.html');
 });
+app.get('/stock', function(request, response) {
+  response.sendFile('/views/index.html');
+});
 
-app.listen(app.get('port'), function() {
+server = app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+io = require('socket.io')(server);
