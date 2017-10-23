10118a0f8c309f88dd3f6568d9993d7e9269aa74
Shaunak Kale <epicshaunak@gmail.com>
2017-10-22 21:14:26 -0700

    hi

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ var index = require('./routes/index');
 var stock = require('./routes/stock');
 
 var app = express();
-
+app.use(allowCrossDomain())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -37,13 +37,7 @@ app.use('/', index);
 app.use('/stock', stock);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
