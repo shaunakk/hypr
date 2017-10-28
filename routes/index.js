b3f18771583ae169a5cfd7da03957912674d32bb
Shaunak Kale <epicshaunak@gmail.com>
2017-10-28 12:17:32 -0700

    hi

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var http = require("http");
+setInterval(function() {
+  http.get("http://hypedesk.herokuapp.com");
+}, 300000);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {title: 'HypeDesk'});
--- routes/stock.js
+++ routes/stock.js
@@ -4,57 +4,66 @@ var request = require('request')
 var numeral = require('numeral');
 const csv = require('csvtojson')
 var stockData = []
+var stockDataDuring = []
 fs = require('fs')
 
 router.get('/', function(req, res, next) {
   res.send(stockData);
 
 });
-var result
-var csvData
-var request = require('request');
-request('http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download', function(error, response, body) {
-  if (error) {
-    console.log(error)
-  }
-  csv({noheader: false}).fromString(body).on('json', (csvRow) => { // this func will be called 3 times
-    stockData.push(csvRow)
-    console.log(csvRow)
-  }).on('done', () => {
-    stockData.forEach((item, index) => {
-
-      if (item.MarketCap != '0' || item.MarketCap != 'n/a') {
-        item.MarketCap = item.MarketCap.replace("$", "")
-        if (item.MarketCap.toString().includes('M')) {
-          item.MarketCap = item.MarketCap.replace("M", "")
-          item.MarketCap = parseFloat(item.MarketCap * 1000000)
-        } else if (item.MarketCap.toString().includes('B')) {
-          item.MarketCap = item.MarketCap.replace("B", "")
-          item.MarketCap = parseFloat(item.MarketCap * 1000000000)
+setInterval(refreshStockData, 5000)
+function refreshStockData() {
+  stockDataDuring = []
+  request('http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download', function(error, response, body) {
+    if (error) {
+      console.log(error)
+    }
+    csv({noheader: false}).fromString(body).on('json', (csvRow) => { // this func will be called 3 times
+      stockDataDuring.push(csvRow)
+    }).on('done', () => {
+      request('http://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nyse&render=download', function(error, response, body) {
+        if (error) {
+          console.log(error)
         }
-        item.MarketCap = parseInt(item.MarketCap * 1000) / 1000
+        csv({noheader: false}).fromString(body).on('json', (csvRow) => { // this func will be called 3 times
+          stockDataDuring.push(csvRow)
+        }).on('done', () => {
+          stockDataDuring.forEach((item, index) => {
 
-        delete item.LastSale
-        delete item['Summary Quote']
-        delete item.field9
-        delete item.IPOyear
-      } else {
-        delete item
-      }
-    });
-    stockData = stockData.filter(x => x)
-    stockData.sort((a, b) => {
-      return a.MarketCap - b.MarketCap
+            if (item.MarketCap.toString().includes('$')) {
+              item.MarketCap = item.MarketCap.replace("$", "")
+            }
+
+            if (item.MarketCap.toString().includes('M')) {
+              item.MarketCap = item.MarketCap.replace("M", "")
+              item.MarketCap = parseFloat(item.MarketCap * 1000000)
+            } else if (item.MarketCap.toString().includes('B')) {
+              item.MarketCap = item.MarketCap.replace("B", "")
+              item.MarketCap = parseFloat(item.MarketCap * 1000000000)
+            }
+            item.MarketCap = parseInt(item.MarketCap * 1000) / 1000
+
+            delete item['Summary Quote']
+            delete item.field9
+            delete item.IPOyear
+          });
+
+          stockDataDuring.reverse()
+          stockDataDuring.sort((a, b) => {
+            return a.MarketCap - b.MarketCap
+          })
+          stockDataDuring.reverse()
+          stockDataDuring.forEach((item, index) => {
+            item.MarketCap = numeral(item.MarketCap).format('0.0a');
+            item.MarketCap = item.MarketCap.replace("m", "M")
+            item.MarketCap = item.MarketCap.replace("b", "B")
+            item.LastSale = "$" + item.LastSale
+          })
+          stockData = stockDataDuring
+        })
+      })
     })
-    stockData.forEach((item, index) => {
-      item.MarketCap = numeral(item.MarketCap).format('0.0a');
-      item.MarketCap = item.MarketCap.replace("m", "M")
-      item.MarketCap = item.MarketCap.replace("b", "B")
-    })
-    stockData.reverse()
-
-  })
-
-});
-
+  });
+}
+refreshStockData()
 module.exports = router;
