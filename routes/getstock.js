107c17cd3ef6b357ec8f531af850217f2a41b925
Shaunak Kale <epicshaunak@gmail.com>
2017-10-28 15:07:36 -0700

    10-28-2017

--- routes/getstock.js
+++ routes/getstock.js
@@ -7,14 +7,14 @@ router.get('/', function(req, res, next) {
   let stockData = {}
   alpha.data.intraday(req.query.stock, 'full', 'json', '15').then(data => {
     stockData.intraday = alpha.util.polish(data);
-    stockData.intradayData = []
+    stockData.y = []
+    stockData.x = []
     for (let stockDate in stockData.intraday['Time Series (15min)']) {
       let formattedDate = moment(stockDate).format('M/D/YY h:mmA');
-      stockData.intradayData.push({'time': formattedDate, 'info': stockData.intraday['Time Series (15min)'][stockDate]
-      })
-      console.log(stockDate)
+      stockData.y.push(formattedDate)
+      stockData.x.push(stockData.intraday['Time Series (15min)'][stockDate].open)
     }
-    delete stockData.intraday['Time Series (15min)']
+    delete stockData.intraday
     res.send(stockData)
   });
 
