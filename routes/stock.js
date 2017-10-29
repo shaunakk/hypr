f2abfe7c7021bf3bd0c13b803484fc44d23b987b
Shaunak Kale <epicshaunak@gmail.com>
2017-10-28 20:27:42 -0700

    10-28-2017

--- routes/stock.js
+++ routes/stock.js
@@ -29,23 +29,24 @@ function refreshStockData() {
           stockDataDuring.push(csvRow)
         }).on('done', () => {
           stockDataDuring.forEach((item, index) => {
+            if ('MarketCap' in item) {
+              if (item.MarketCap.toString().includes('$')) {
+                item.MarketCap = item.MarketCap.replace("$", "")
+              }
 
-            if (item.MarketCap.toString().includes('$')) {
-              item.MarketCap = item.MarketCap.replace("$", "")
+              if (item.MarketCap.toString().includes('M')) {
+                item.MarketCap = item.MarketCap.replace("M", "")
+                item.MarketCap = parseFloat(item.MarketCap * 1000000)
+              } else if (item.MarketCap.toString().includes('B')) {
+                item.MarketCap = item.MarketCap.replace("B", "")
+                item.MarketCap = parseFloat(item.MarketCap * 1000000000)
+              }
+              item.MarketCap = parseInt(item.MarketCap * 1000) / 1000
+
+              delete item['Summary Quote']
+              delete item.field9
+              delete item.IPOyear
             }
-
-            if (item.MarketCap.toString().includes('M')) {
-              item.MarketCap = item.MarketCap.replace("M", "")
-              item.MarketCap = parseFloat(item.MarketCap * 1000000)
-            } else if (item.MarketCap.toString().includes('B')) {
-              item.MarketCap = item.MarketCap.replace("B", "")
-              item.MarketCap = parseFloat(item.MarketCap * 1000000000)
-            }
-            item.MarketCap = parseInt(item.MarketCap * 1000) / 1000
-
-            delete item['Summary Quote']
-            delete item.field9
-            delete item.IPOyear
           });
 
           stockDataDuring.reverse()
