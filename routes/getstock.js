766ea0a5fbaef9fc941def4d30ea025af974cda6
Shaunak Kale <epicshaunak@gmail.com>
2017-10-29 09:58:08 -0700

    10-29-2017

--- routes/getstock.js
+++ routes/getstock.js
@@ -5,15 +5,15 @@ const moment = require('moment')
 const alpha = require('alphavantage')({key: '9322BZUXBXU4IQMF'});
 router.get('/', function(req, res, next) {
   let stockData = {}
-  alpha.data.intraday(req.query.stock, 'full', 'json', '15').then(data => {
+  alpha.data.intraday(req.query.stock, 'full', 'json', '60').then(data => {
     stockData.intraday = alpha.util.polish(data);
     console.log(stockData.intraday)
     stockData.y = []
     stockData.x = []
-    for (let stockDate in stockData.intraday['Time Series (15min)']) {
+    for (let stockDate in stockData.intraday['Time Series (60min)']) {
       let formattedDate = moment(stockDate).format('MMM D, YYYY h:mmA');
       stockData.y.push(formattedDate)
-      stockData.x.push(parseFloat(stockData.intraday['Time Series (15min)'][stockDate].open).toFixed(2))
+      stockData.x.push(parseFloat(stockData.intraday['Time Series (60min)'][stockDate].open).toFixed(2))
     }
     stockData.x.reverse()
     stockData.y.reverse()
